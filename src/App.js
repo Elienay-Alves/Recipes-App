fcc653bed0ef26ee0bec93c6dc924c3ed669b770
Kaique Coelho <94154469+KaiqueCoelho@users.noreply.github.com>
2022-04-26 16:42:08 -0300

    Revert "[GRUPO 19]Adiciona pagina de login e testes requisito 2"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
 
 function App() {
   return (
