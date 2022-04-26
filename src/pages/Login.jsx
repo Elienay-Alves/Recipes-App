096be80c7e014a56a43f0b016e84605684451edb
Kaue Alves <kaue_alves00@yahoo.com>
2022-04-26 17:45:40 -0300

    testa se o email é válido e se a senha tem mais de 6 caracteres - requisito 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const inputsValidate = () => {
+    const regex = /\S+@\S+\.\S+/;
+    const emailValidation = regex.test(email);
+    const MIN_LENGTH = 5;
+    const passwordValidation = password.length > MIN_LENGTH;
+    const validate = emailValidation && passwordValidation;
+    console.log(password.length);
+    setDisabled(!validate);
+  };
+
   return (
     <main>
       <h1>Login</h1>
-      <input type="text" data-testid="email-input" />
-      <input type="password" data-testid="password-input" />
-      <button type="button" data-testid="login-submit-btn">Enter</button>
+      <input
+        type="text"
+        data-testid="email-input"
+        value={ email }
+        onChange={ (e) => {
+          setEmail(e.target.value);
+          inputsValidate();
+        } }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        value={ password }
+        onChange={ (e) => {
+          setPassword(e.target.value);
+          inputsValidate();
+        } }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ disabled }
+      >
+        Enter
+      </button>
     </main>
   );
 }
