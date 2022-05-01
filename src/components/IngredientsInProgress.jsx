e7bfc588192fff79751724b9cfb88f421f2149ba
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-05-01 17:35:34 -0300

    Funciona mas não passa no teste rsrssr acredito que seja pelo fato de estar fazendo requisição na hora que abre a página de progress

--- src/components/IngredientsInProgress.jsx
+++ src/components/IngredientsInProgress.jsx
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 import FinishRecipeBtn from './FinishRecipeBtn';
 
 const INGREDIENTS_NUMBER = 20;
+const inProgressStorage = {
+  cocktails: {
+  },
+  meals: {
+  },
+};
 function IngredientsInProgress({ meal = {}, drink = {} }) {
   const [ingredientsInProgress, setIngredientsInProgress] = useState([]);
-  const [savedProgress, setSavedProgress] = useState([]);
+  const [savedProgress, setSavedProgress] = useState(inProgressStorage);
   const [doneIngredients, setDoneIngredients] = useState([]);
   const isMeal = Object.keys(meal).length > 0;
   const isDrink = Object.keys(drink).length > 0;
@@ -30,19 +36,26 @@ function IngredientsInProgress({ meal = {}, drink = {} }) {
     }
   };
 
-  const inProgressStorage = {
-    cocktails: {
-    },
-    meals: {
-    },
-  };
-
   useEffect(() => {
     getIngredientsInProgress();
-    const savedItems = JSON.parse(localStorage.getItem('inProgressRecipes'))
-    || localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressStorage));
-    setSavedProgress(savedItems || inProgressStorage);
+    const savedItems = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (savedItems) {
+      setSavedProgress(savedItems);
+    } else {
+      localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressStorage));
+    }
   }, []);
+
+  useEffect(() => {
+    if (savedProgress) {
+      if (isMeal) {
+        setDoneIngredients(savedProgress.meals[recipeId] || []);
+      } else if (isDrink) {
+        setDoneIngredients(savedProgress.cocktails[recipeId] || []);
+      }
+    }
+  }, [savedProgress]);
+
   const handleIngredient = (e) => {
     if (e.target.checked) {
       e.target.parentNode.className = 'markedIngredient';
@@ -53,13 +66,10 @@ function IngredientsInProgress({ meal = {}, drink = {} }) {
         .filter((ingredient) => ingredient !== e.target.name);
       setDoneIngredients(ingredientsSet);
     }
-    if (isMeal) {
-      // const prevProgress = savedProgress.meals[recipeId] || [];
-    }
   };
 
   useEffect(() => {
-    if (doneIngredients.length > 0) {
+    if (savedProgress) {
       if (isMeal) {
         savedProgress.meals[recipeId] = doneIngredients;
       } else if (isDrink) {
@@ -69,11 +79,6 @@ function IngredientsInProgress({ meal = {}, drink = {} }) {
     }
   }, [doneIngredients]);
 
-  // const handleChecked = () => {
-  //   console.log(savedProgress.meals[recipeId]);
-  //   return true;
-  // };
-
   return (
 
     <>
@@ -82,11 +87,17 @@ function IngredientsInProgress({ meal = {}, drink = {} }) {
       </h3>
       <ul>
         {ingredientsInProgress.map((ingredient, index) => (
-          <li key={ index } data-testid={ `${index + 1}-ingredient-step` }>
+          <li
+            key={ index }
+            data-testid={ `${index}-ingredient-step` }
+            className={ doneIngredients?.includes(JSON.stringify(index))
+              ? 'markedIngredient' : '' }
+          >
             <input
               type="checkbox"
               name={ index }
-              onClick={ handleIngredient }
+              checked={ doneIngredients.includes(JSON.stringify(index)) }
+              onChange={ handleIngredient }
             />
             {ingredient}
           </li>
