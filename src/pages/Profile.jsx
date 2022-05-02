24af1e4adf7684082a153e30ab72c8380c87d127
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-05-02 01:48:20 -0300

    adiciona email do usuario na tela e botões: done recipes, favorite recipes, logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    setEmail(JSON.parse(localStorage.getItem('user')).email);
+  }, []);
+
   return (
     <>
       <Header title="Profile" />
-      <h1>Profile</h1>
+      <section>
+        <h2 data-testid="profile-email">{ email }</h2>
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button type="button" data-testid="profile-logout-btn">Logout</button>
+      </section>
       <Footer />
     </>
   );
