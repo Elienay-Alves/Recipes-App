9f711da4422918bbe11f3f9ee974ea2316099156
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-05-02 01:55:46 -0300

    adiciona redirecionamento de rotas para os botões

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -9,14 +10,32 @@ function Profile() {
     setEmail(JSON.parse(localStorage.getItem('user')).email);
   }, []);
 
+  const handleLogout = () => {
+    localStorage.clear();
+  };
+
   return (
     <>
       <Header title="Profile" />
       <section>
         <h2 data-testid="profile-email">{ email }</h2>
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-        <button type="button" data-testid="profile-logout-btn">Logout</button>
+        <Link to="/done-recipes">
+          <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button type="button" data-testid="profile-favorite-btn">
+            Favorite Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ handleLogout }
+          >
+            Logout
+          </button>
+        </Link>
       </section>
       <Footer />
     </>
