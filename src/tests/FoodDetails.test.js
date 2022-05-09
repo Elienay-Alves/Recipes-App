64aa88a00d7fb1c9247956f80f68322714fed39d
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-05-09 17:59:28 -0300

    adiciona muito teste git add .git add . 90% falso

--- src/tests/FoodDetails.test.js
+++ src/tests/FoodDetails.test.js
@@ -5,29 +5,20 @@ import renderWithRouter from './renderWithRouter';
 import FoodDetails from '../pages/FoodDetails';
 import DetailsHeader from '../components/DetailsHeader';
 import DetailsPageBtn from '../components/DetailsPageBtn';
-import App from '../App';
-
-const mockkFavorite = [{
-  alcoholicOrNot: '',
-  category: 'Side',
-  idMeal: 52977,
-  strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-  strMeal: 'Corba',
-  nationality: 'Turkish',
-  type: 'food',
-},
-{
-  alcoholicOrNot: 'Alcoholic',
-  category: 'Cocktail',
-  id: '17222',
-  image: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
-  name: 'A1',
-  nationality: '',
-  type: 'drink',
-}];
 
 describe('Testa o componente FoodDetails', () => {
   beforeEach(() => {
+    jest.clearAllMocks();
+    localStorage.clear();
+  });
+
+  test('Se o componente possui um titulo', () => {
+    renderWithRouter(<FoodDetails />);
+    const title = screen.getByRole('heading', { level: 1 });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Se o componente DetailsHeader possui os elementos esperados', () => {
     const mockFavorite = [{
       alcoholicOrNot: '',
       category: 'Side',
@@ -47,15 +38,6 @@ describe('Testa o componente FoodDetails', () => {
       type: 'drink',
     }];
     global.localStorage.setItem('favoriteRecipes', JSON.stringify(mockFavorite));
-  });
-
-  test('Se o componente possui um titulo', () => {
-    renderWithRouter(<FoodDetails />);
-    const title = screen.getByRole('heading', { level: 1 });
-    expect(title).toBeInTheDocument();
-  });
-
-  test('Se o componente DetailsHeader possui os elementos esperados', () => {
     Object.defineProperty(navigator, 'clipboard', {
       value: {
         writeText: () => {},
@@ -71,6 +53,25 @@ describe('Testa o componente FoodDetails', () => {
   });
 
   test('Se o componente DetailsPageBtn possui os elementos esperados', () => {
+    const mockFavorite = [{
+      alcoholicOrNot: '',
+      category: 'Side',
+      id: 52977,
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+      name: 'Corba',
+      nationality: 'Turkish',
+      type: 'food',
+    },
+    {
+      alcoholicOrNot: 'Alcoholic',
+      category: 'Cocktail',
+      id: '17222',
+      image: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+      name: 'A1',
+      nationality: '',
+      type: 'drink',
+    }];
+    global.localStorage.setItem('favoriteRecipes', JSON.stringify(mockFavorite));
     jest.spyOn(Object.getPrototypeOf(window.localStorage), 'getItem');
     Object.setPrototypeOf(window.localStorage.getItem, jest.fn());
     const { customHistory } = renderWithRouter(<DetailsPageBtn />);
@@ -80,22 +81,5 @@ describe('Testa o componente FoodDetails', () => {
     userEvent.click(startRecipe);
     expect(customHistory.location.pathname).toBe('/drinks/undefined/in-progress');
     expect(localStorage.getItem).toHaveBeenCalled();
-  });
-
-  test('Se o componente DetailsPageBtn renderiza as instruções', async () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve(mockkFavorite),
-    }));
-    renderWithRouter(<App />);
-    const emailInput = screen.getByTestId('email-input');
-    const email = 'trybe@trybe.com';
-    const passwordInput = screen.getByTestId('password-input');
-    const password = '1234567';
-    const button = screen.getByTestId('login-submit-btn');
-    userEvent.type(passwordInput, password);
-    userEvent.type(emailInput, email);
-    userEvent.click(button);
-    const corba = await screen.findByText(/Corba/i);
-    expect(corba).toBeInTheDocument();
   });
 });
